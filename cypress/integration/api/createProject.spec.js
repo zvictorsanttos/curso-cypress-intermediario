f81a4f8f1f581a5aa37cfaecb1d5fd44dde2e67d
João Victor da Silva Santos <vitinn_santos@hotmail.com>
2021-06-23 00:23:07 -0300

    Implementando teste de API

---
+++ cypress/integration/api/createProject.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+
+const faker = require("faker");
+
+describe("Create Project", () => {
+  it("Projeto Criado com sucesso", () => {
+    const project = {
+      name: `project-${faker.random.uuid()}`,
+      description: faker.random.words(5),
+    };
+    cy.api_createProject(project).then((response) => {
+      expect(response.status).to.equal(201);
+      expect(response.body.name).to.equal(project.name);
+      expect(response.body.description).to.equal(project.description);
+    });
+  });
+});
--- cypress/integration/gui/createIssue.spec.js
+++ cypress/integration/gui/createIssue.spec.js
@@ -14,7 +14,7 @@ describe("Create Issue", () => {
 
   before(() => {
     cy.login();
-    cy.gui_createProject(issue.project);
+    cy.api_createProject(issue.project);
   });
   it("Criar Issue com sucesso", () => {
     cy.gui_createIssue(issue);
